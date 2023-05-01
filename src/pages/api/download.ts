fd86dea963635cfca8b84da1c97c6f5ea5b83368
Ronier Lima <ronier.lim@gmail.com>
2023-05-01 15:09:00 -0300

    corrige enviando resto dos arquivos

--- src/pages/api/download.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch";
-
-type Data = {
-  name: string;
-};
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#191d2b',
+        'secondary': '#8E8EED',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
